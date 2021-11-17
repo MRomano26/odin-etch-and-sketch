359eeb9136323080bd140f719158253153256150
MRomano26 <michael.mar26@gmail.com>
2021-11-17 14:26:00 -0500

    Add reset button that resets grid with new dimensions

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ container = document.querySelector(".container");
 
 function createGrid(dimension) {
     containerWidth = 450;
-    gridBoxWidth = Math.floor(containerWidth/dimension);
+    gridBoxWidth = Math.floor(containerWidth/dimension*100)/100;
 
     for (i = 0; i < dimension**2; i++) {
         gridBox = document.createElement("div");
@@ -13,14 +13,35 @@ function createGrid(dimension) {
     }
 }
 
+function addListeners() {
+    //add event listeners
+    boxes = document.querySelectorAll(".box");
+
+    boxes.forEach((box) => {
+        box.addEventListener('mouseenter', e => {
+            box.className = "box coloured";
+        })
+    })
+}
+
+function reset() {
+    boxes = document.querySelectorAll(".box");
+
+    boxes.forEach((box) => {
+        box.parentNode.removeChild(box);
+    })
+    while (true) {
+        dimension = prompt("Enter dimensions of new grid.");
+        if (dimension < 51) {
+            break;
+        }
+        alert("Too large")
+    }
+    
+    createGrid(dimension);
+    addListeners();
+}
+
 //initial 16x16 grid
 createGrid(16);
-
-//add event listeners
-boxes = document.querySelectorAll(".box");
-
-boxes.forEach((box) => {
-    box.addEventListener('mouseenter', e => {
-        box.className = "coloured";
-    })
-})
+addListeners();
